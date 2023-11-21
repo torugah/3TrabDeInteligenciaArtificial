420e4109092c7661203a1dfe26a04ee374375e87
torugah <fillipibarros@gmail.com>
2023-11-21 00:01:46 -0300

    Ajuste no Controller para List<String>

--- src/main/java/theHexagon/controllers/HexagonController.java
+++ src/main/java/theHexagon/controllers/HexagonController.java
@@ -1,11 +1,10 @@
 package theHexagon.controllers;
 
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +18,11 @@ import theHexagon.Methods;
 @RequestMapping(value = "/program")
 @CrossOrigin(origins = {"http://localhost:8080",
                         "http://localhost:5500",
+                        "http://127.0.0.1:5500",
                         "http://127.0.0.1:5500/index.html",
-                        "http://127.0.0.1:5500"}, allowedHeaders = {"Authorization"})
+                        "http://127.0.0.1:5501",
+                        "http://localhost:5501",
+                        "http://127.0.0.1:5500/relatorio.html"}, allowedHeaders = {"Authorization"})
 
 public class HexagonController {
 
@@ -35,7 +37,7 @@ public class HexagonController {
     http://localhost:8080/program/returnTable/12/800/0.05/0.85/825/0.01/0.87/850/0.04/0.89/875/0.03/0.91/900/0.02/0.93
     */
     @GetMapping(value = "/returnTable/{tentativa}/{ti1}/{tf1}/{fr1}/{ti2}/{tf2}/{fr2}/{ti3}/{tf3}/{fr3}/{ti4}/{tf4}/{fr4}/{ti5}/{tf5}/{fr5}")
-    public ResponseEntity<String> retornaTabela(
+    public List<String> retornaTabela(
         @PathVariable int tentativa,
         @PathVariable double ti1,
         @PathVariable double tf1,
@@ -51,30 +53,55 @@ public class HexagonController {
         @PathVariable double fr4,
         @PathVariable double ti5,
         @PathVariable double tf5,
-        @PathVariable double fr5
-    ){
+        @PathVariable double fr5){
         String table = Methods.realizarMetodosParaTabela(tentativa, ti1, tf1, fr1,
                                                             ti2, tf2, fr2,
                                                             ti3, tf3, fr3, 
                                                             ti4, tf4, fr4, 
                                                             ti5, tf5, fr5);
-        return ResponseEntity.ok(table);
+
+        // Separar os valores por vírgula
+        String[] valoresSeparados = table.split(",");
+
+        // Criar um array de doubles para armazenar os valores convertidos
+        double[] arrayValores = new double[valoresSeparados.length];
+
+        // Converter e armazenar os valores no array
+        for (int i = 0; i < valoresSeparados.length; i++) {
+            arrayValores[i] = Double.parseDouble(valoresSeparados[i]);
+        }
+
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+
+        List<String> response = new ArrayList<>();
+        response.add("Método — Dados Fornecidos — Ganhos");
+        response.add("SE    " + String.valueOf(df.format(arrayValores[0])) + "%");
+        response.add("SEA — Tentativas: " + tentativa + " — " + String.valueOf(df.format(arrayValores[1])) + "%");
+        response.add("SEA — Tentativas: " + tentativa/2 + " — " +String.valueOf(df.format(arrayValores[2])) + "%");
+        response.add("SEA — Tentativas: " + tentativa/4 + " — " +String.valueOf(df.format(arrayValores[3])) + "%");
+        response.add("TS  — Temp. Inicial: " + ti1 +"   Temp. Final: "+ tf1 +"   Fator Red.: "+ fr1 + " — " + String.valueOf(df.format(arrayValores[4])) + "%");
+        response.add("TS  — Temp. Inicial: " + ti2 +"   Temp. Final: "+ tf2 +"   Fator Red.: "+ fr2 + " — " + String.valueOf(df.format(arrayValores[5])) + "%");
+        response.add("TS  — Temp. Inicial: " + ti3 +"   Temp. Final: "+ tf3 +"   Fator Red.: "+ fr3 + " — " + String.valueOf(df.format(arrayValores[6])) + "%");
+        response.add("TS  — Temp. Inicial: " + ti4 +"   Temp. Final: "+ tf4 +"   Fator Red.: "+ fr4 + " — " + String.valueOf(df.format(arrayValores[7])) + "%");
+        response.add("TS  — Temp. Inicial: " + ti5 +"   Temp. Final: "+ tf5 +"   Fator Red.: "+ fr5 + " — " + String.valueOf(df.format(arrayValores[8])) + "%");
+
+        return response;
     }
 
     @GetMapping(value = "/returnPercentege", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, String>> retorneOsGanhos() {
+    public List<String> retorneOsGanhos() {
         DecimalFormat df = new DecimalFormat("#,##0.00");
 
         String g1 = df.format(Methods.ganhoComSubidaDeEncostaP());
         String g2 = df.format(Methods.ganhoComSubidaDeEncostaAlteradaP());
         String g3 = df.format(Methods.ganhoComTemperaSimuladaP());
 
-        Map<String, String> response = new HashMap<>();
-        response.put("Ganhos Com Subida De Encosta", g1);
-        response.put("Ganhos Com Subida De Encosta Alterada", g2);
-        response.put("Ganhos Com Tempera Simulada", g3);
+        List<String> response = new ArrayList<>();
+        response.add("Ganhos Com Subida De Encosta -> " + g1 + "%");
+        response.add("Ganhos Com Subida De Encosta Alterada -> " + g2 + "%");
+        response.add("Ganhos Com Tempera Simulada -> " + g3 + "%");
 
-        return ResponseEntity.ok(response);
+        return response;
     }
 
 
